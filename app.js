6735318cbe5776c7a3982903c64bcd54e9390eb4
Truong Dao <truong.dao.do@gmail.com>
2020-11-30 20:28:05 -0500

    Notes until lesson 33

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const path = require('path');
+//omitting slash or period, will look for global module.
+const http = require('http');
 
+// Anonymous function takes two argument, request(req) and response(res)
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
 
-const app = express();
+});
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-
-app.listen(3000);
+//This will keep this running for a coing request. The first argument is the port.
+server.listen(3000)
