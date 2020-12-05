081e1e4428b3a922519c93111f5c7fbba3119b51
Truong Dao <truong.dao.do@gmail.com>
2020-12-05 14:04:21 -0500

    Lesson 33

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 //omitting slash or period, will look for global module.
 const http = require('http');
+const fs = require("fs");
 
 // Anonymous function takes two argument, request(req) and response(res)
 const server = http.createServer((req, res) => {
     // console.log(req);
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
+    if (url ==='/message' && method === "POST") {
+        // uses the file system to save a POST request.
+        fs.writeFileSync("message.txt", 'DUMMY');
+        // Setting the status to a redirection
+        res.statusCode = 302;
+        res.setHeader("Location", '/');
+        return res.end();
+    }
 });
 
 //This will keep this running for a coing request. The first argument is the port.
