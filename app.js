01f980a14c9e28a756560fb18c6823bc47c0bfa9
Truong Dao <truong.dao.do@gmail.com>
2020-12-12 15:38:28 -0500

    Lesson65

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const http = require('http');
 const express = require("express")
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("I'm the first middleware");
-    next(); // Next allow request to continu to the next code of line(middleware).
+// Registers a middleware, will parse the body passed in the form
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product", (req, res, next) => {
+    // console.log("I'm another middleware");
+    res.send('<Form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></Form>');
 });
 
-app.use((req, res, next) => {
-    console.log("I'm another middleware");
+app.post("/product", (req, res, next) => {
+    // extracting the form
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.use("/", (req, res, next) => {
+    res.send("<h1>Hello from Express!</h1>");
+    // console.log("I'm a middleware");
 });
 
 const server = http.createServer(app);
