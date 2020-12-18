4307ca87d1774ebce1c13f480e2655f7780a0c60
Truong Dao <truong.dao.do@gmail.com>
2020-12-18 17:42:34 -0500

    Lesson 67

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoute);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
+})
 
 const server = http.createServer(app);
 
