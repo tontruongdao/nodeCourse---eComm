70217fdbcaea9ed4d9197240e48b621a63e77674
Truong Dao <truong.dao.do@gmail.com>
2020-12-18 20:03:00 -0500

    Lesson 71

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require("express")
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const adminRoute = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
@@ -14,7 +15,7 @@ app.use("/admin", adminRoute);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
 
 const server = http.createServer(app);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
-const express = require("express")
+const express = require("express");
+const path = require('path');
 
 const router = express.Router();
 
 // "get" uses an exact match instead of a path that starts with the first argument.
 router.get("/", (req, res, next) => {
-    res.send("<h1>Hello from Express!</h1>");
+    // res.send("<h1>Hello from Express!</h1>");
     // console.log("I'm a middleware");
+
+    // This will select the absolute path of our machine, we resolve this by importing the "path" module.
+    // "__dirname" selected our root folder.
+    // The join method will concatenate the different arguments, since this can we read by Linux and iOS.
+    res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
