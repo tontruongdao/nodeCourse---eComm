ac5770bc55cd0cdcab12c762cdbe28297d10acde
Truong Dao <truong.dao.do@gmail.com>
2021-01-09 15:55:23 -0500

    Lesson92

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get("/add-product", (req, res, next) => {
     // console.log("I'm another middleware");
     // res.send('<Form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></Form>');
     // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // This is reached throught "/admin/add-product" => POST request
