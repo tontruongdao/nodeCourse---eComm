5ca2e03cae42a75228430d41fc85cd620c6a46d3
Truong Dao <truong.dao.do@gmail.com>
2021-01-14 20:44:49 -0500

    Lesson102

--- models/product.js
+++ models/product.js
@@ -1,7 +1,22 @@
 const fs = require('fs'); // Core file system package
 const path = require('path'); // Core path package 
 
-const products = [];
+const p = path.join(
+    path.dirname(process.mainModule.filename), // Join takes 3 argument
+    'data', // In a data folder
+    'products.json' // In a json file
+); 
+
+const getProductsFromFile = (cb) => {
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        } else {
+            return cb(JSON.parse(fileContent));
+        }
+    })
+}
 
 module.exports = class Product {
     
@@ -11,41 +26,25 @@ module.exports = class Product {
 
     // This function reads our file in the path and adds the saved item whether it countains something or not.
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename), // Join takes 3 argument
-            'data', // In a data folder
-            'products.json' // In a json file
-        ); 
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent); // Takes incoming JSON and convert a JS array or object
-            }
-
+        getProductsFromFile(products => {
             products.push(this); // This will lose this content and will not refer to the class if we do not use an arrow function
                                  // Adds content to our file
-
+    
             fs.writeFile(p, JSON.stringify(products), (err) => { // Converts to JSON and updates our file
                 console.log(err)
             }) 
-        })
+        });
+
+        // fs.readFile(p, (err, fileContent) => {
+            // let products = [];
+            // if (!err) {
+            //     products = JSON.parse(fileContent); // Takes incoming JSON and convert a JS array or object
+            // }
+        // })
     }
 
     // This fetches all our saved items.
     static fetchAll(cb) { // Used callback to make our code asynchronous
-
-        const p = path.join(
-            path.dirname(process.mainModule.filename), // Join takes 3 argument
-            'data', // In a data folder
-            'products.json' // In a json file
-        ); 
-
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                return cb([]);
-            } 
-            return cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb);
     }
 }
