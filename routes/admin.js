23677efe0397cfed8bab6043a77c8816d14837b5
Truong Dao <truong.dao.do@gmail.com>
2021-01-25 20:28:30 -0500

    Corrected delete bug

--- routes/admin.js
+++ routes/admin.js
@@ -18,7 +18,7 @@ router.post("/add-product", adminController.postAddProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
-router.post('/delete', adminController.postDeleteProduct);
+router.post('/delete-product', adminController.postDeleteProduct);
 
 // module.exports = router; // Single export in the file
 module.exports = router;
